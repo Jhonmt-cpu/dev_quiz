7a9334f052165564f8997f716aa330709c4c68b6
João Mateus <joaomateusabm@yahoo.com.br>
2021-04-23 20:46:22 -0300

    feat: finished the third class if the nlw#5

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,7 +1,12 @@
+import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/home_state.dart';
 import 'package:dev_quiz/home/widgets/app_bar/app_bar_widget.dart';
 import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -11,55 +16,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(height: 40),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              physics: BouncingScrollPhysics(),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    label: "Fácil",
-                  ),
-                  SizedBox(width: 5),
-                  LevelButtonWidget(
-                    label: "Médio",
-                  ),
-                  SizedBox(width: 5),
-                  LevelButtonWidget(
-                    label: "Difícil",
-                  ),
-                  SizedBox(width: 5),
-                  LevelButtonWidget(
-                    label: "Perito",
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 24),
-            Expanded(
-              child: GridView.count(
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                crossAxisCount: 2,
-                children: [
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                ],
-              ),
-            )
-          ],
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(
+                      label: "Fácil",
+                    ),
+                    SizedBox(width: 5),
+                    LevelButtonWidget(
+                      label: "Médio",
+                    ),
+                    SizedBox(width: 5),
+                    LevelButtonWidget(
+                      label: "Difícil",
+                    ),
+                    SizedBox(width: 5),
+                    LevelButtonWidget(
+                      label: "Perito",
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+              Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  crossAxisCount: 2,
+                  children: controller.quizzes!
+                      .map((e) => QuizCardWidget(
+                            title: e.title,
+                            porcentage: e.questionAnswered / e.questions.length,
+                            completed:
+                                "${e.questionAnswered}/${e.questions.length}",
+                          ))
+                      .toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
   }
 }
